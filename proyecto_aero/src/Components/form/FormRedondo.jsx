c2a267ca4e931d0cc2405343fbe0afade3980771
Paola <paolandrea96@gmail.com>
2023-05-28 16:53:08 -0500

    Home styles

--- proyecto_aero/src/Components/form/FormRedondo.jsx
+++ proyecto_aero/src/Components/form/FormRedondo.jsx
@@ -65,9 +65,9 @@ const showPeople = [
 //         peopleCopy[index].number--;
 //         setPeople([...peopleCopy]);
 //     }
-    // const handlePluss = (index) => {
-    //     if ()
-    // }
+// const handlePluss = (index) => {
+//     if ()
+// }
 // }
 const validationSchema = Yup.object().shape({
     origen: Yup.string()
@@ -215,15 +215,15 @@ const FormRedondo = ({ origen, destino, salida, regreso, pasajeros, handleVuelo 
                             <legend> Busca un nuevo destino y comienza la aventura. </legend>
                         </FormControl>
                         {/* <FormControl w="25%" > */}
-                        <FormLabel className ="form__label" >Descubre vuelos al mejor precio y perfectos para cualquier viaje </FormLabel>
+                        <FormLabel className="form__label" >Descubre vuelos al mejor precio y perfectos para cualquier viaje </FormLabel>
                         <Stack className="form__selectVuelo" direction='row' spacing={7} w="50%" >
 
-                            <span onClick={selectVueloSencillo} >Viaje sencillo </span>
+                            <span onClick={selectVueloSencillo}>Viaje sencillo </span>
                             <span onClick={selectVueloRedondo}>Viaje redondo</span>
                         </Stack>
 
-                        <Stack direction='row' spacing={4} w="90%" className ="form__selectCountry">
-                            <select value={formik.values.origen} onChange={formik.handleChange} name="origen" className ="form__selectOrigin">
+                        <Stack direction='row' spacing={4} w="90%" className="form__selectCountry">
+                            <select value={formik.values.origen} onChange={formik.handleChange} name="origen" className="form__selectOrigin">
 
                                 <option > Origen </option>
                                 {infoCities.length &&
@@ -240,7 +240,7 @@ const FormRedondo = ({ origen, destino, salida, regreso, pasajeros, handleVuelo 
 
                             {/* <FormControl w="25%"> */}
 
-                            <select value={formik.values.destino} onChange={formik.handleChange} name="destino" className ="form__selectDestination">
+                            <select value={formik.values.destino} onChange={formik.handleChange} name="destino" className="form__selectDestination">
                                 <option> Selecciona un destino </option>
                                 {/* <FormErrorMessage>{formik.touched.destino && formik.errors.destino && <div>{formik.errors.destino}</div>}</FormErrorMessage> */}
                                 {infoCities.length &&
@@ -254,13 +254,13 @@ const FormRedondo = ({ origen, destino, salida, regreso, pasajeros, handleVuelo 
                             {formik.touched.destino && formik.errors.destino && <div>{formik.errors.destino}</div>}
                         </Stack>
 
-                        <Stack direction='row' spacing={4} w="85%" className ="form__inputsDate">
-                            <Input type="date" className ="form__inputSalida" placeholder="Salida" value={formik.values.salida} onChange={formik.handleChange} name="salida" />
+                        <Stack direction='row' spacing={4} w="85%" className="form__inputsDate">
+                            <Input type="date" className="form__inputSalida" placeholder="Salida" value={formik.values.salida} onChange={formik.handleChange} name="salida" />
                             {/* {formik.touched.salida && formik.errors.salida && <div>{formik.errors.salida}</div>} */}
                             {/* <FormErrorMessage>{formik.touched.salida && formik.errors.salida && <div>{formik.errors.salida}</div>}</FormErrorMessage> */}
 
 
-                            <Input type="date" className ="form__inputRegreso" value={tipoDeVuelo ? '' : formik.values.regreso} onChange={formik.handleChange} disabled={tipoDeVuelo} placeholder="Regreso" name="regreso" id="inputR" />
+                            <Input type="date" className="form__inputRegreso" value={tipoDeVuelo ? '' : formik.values.regreso} onChange={formik.handleChange} disabled={tipoDeVuelo} placeholder="Regreso" name="regreso" id="inputR" />
 
                             <FormErrorMessage>{formik.touched.regreso && formik.errors.regreso && <div>{formik.errors.regreso}</div>}</FormErrorMessage>
 
@@ -268,7 +268,7 @@ const FormRedondo = ({ origen, destino, salida, regreso, pasajeros, handleVuelo 
                         </Stack>
 
                         {/* {formik.touched.regreso && formik.errors.regreso && <div>{formik.errors.regreso}</div>} */}
-                        <Stack direction='row' spacing={4} w="90%">
+                        <Stack direction='row' spacing={4} w="90%" className="form__selectPassengers">
                             <select value={formik.values.pasajeros} onChange={formik.handleChange} w="40%" name="pasajeros">
                                 <option value="."> Pasajeros </option>
                                 <option value="1"> 1 </option>
@@ -276,7 +276,7 @@ const FormRedondo = ({ origen, destino, salida, regreso, pasajeros, handleVuelo 
                                 <option value="3"> 3 </option>
                             </select>
                             {/* <div> */}
-                                {/* <h5> Adultos </h5>
+                            {/* <h5> Adultos </h5>
                                 <section></section>
                             </div>
                             <div>
@@ -286,7 +286,7 @@ const FormRedondo = ({ origen, destino, salida, regreso, pasajeros, handleVuelo 
                             <div>
                                 <h5> Bebes </h5>
                                 <section></section> */}
-                                {/* {
+                            {/* {
                                     people.length && (people.map((item, index) => (
                                         <div key={index}>
                                             <h5> {item.name} </h5>
@@ -299,12 +299,12 @@ const FormRedondo = ({ origen, destino, salida, regreso, pasajeros, handleVuelo 
                                         </div>
                                     )))
                                 } */}
-                                {formik.touched.pasajeros && formik.errors.pasajeros && <div>{formik.errors.pasajeros}</div>}
-                                {/* <FormErrorMessage>{formik.touched.pasajeros&& formik.errors.pasajeros&& <div>{formik.errors.pasajeros}</div>}</FormErrorMessage> */}
+                            {formik.touched.pasajeros && formik.errors.pasajeros && <div>{formik.errors.pasajeros}</div>}
+                            {/* <FormErrorMessage>{formik.touched.pasajeros&& formik.errors.pasajeros&& <div>{formik.errors.pasajeros}</div>}</FormErrorMessage> */}
                             {/* </div>   */}
-                            <Input placeholder="Tienes un código de promoción" w="40%"/> 
+                            <Input placeholder="Tienes un código de promoción" w="60%" />
                         </Stack>
-                        
+
                         <Button className="form__button" type="submit" disabled={formik.isSubmitting} leftIcon={<TbPlaneTilt />} colorScheme='teal' variant='outline' >
                             <figure>
                                 <img>
@@ -318,44 +318,100 @@ const FormRedondo = ({ origen, destino, salida, regreso, pasajeros, handleVuelo 
                     </Flex>
                 </form>
             </section>
-            <section className="mainHome__containerCards">
-                <SimpleGrid spacing={4} w="100%" templateColumns='repeat(auto-fill, minmax(200px, 1fr))'>
-                    <Card>
+            <section className="mainHome__pay" w="90%">
+                <h2> Pago seguro </h2>
+                <Stack className="mainHome__pay__container" w="95%"display="flex" direction='row'>
+                    <div className="mainHome__pay__container__cards">
+                        <span>Tarjeta de credito, tarjeta de debito y pago electrónico</span>
+                        <Stack className="container__cards" direction='row' spacing={4} w="50%">
+                        <img className="card_amexcard" src="https://cms.volaris.com/globalassets/1nextgen/payments/amex.svg" alt="american express card" />
+                            <img className="card_paypal" src=" https://cms.volaris.com/globalassets/1nextgen/payments/paypal.svg" alt="paypal card" />
+                            <img className="card_invex" src=" https://cms.volaris.com/globalassets/1nextgen/payments/invex.svg" alt="invez card" />
+                            <img className="card_mastercard" src="https://cms.volaris.com/globalassets/1nextgen/payments/mastercard.svg" alt="master card card" />
+                            <img className="card_visa" src="https://cms.volaris.com/globalassets/1nextgen/payments/visa.svg" alt="visa card" />
+                        </Stack>
+                    </div>
+                    <div className="mainHome__pay__container__cards">
+                        <span>Efectivo en cualquiera de las sucursales participantes</span>
+                        <Stack className="container__cards" direction='row' spacing={4} w="50%">
+                            <img className="card__oxo" src="https://cms.volaris.com/globalassets/1nextgen/payments/oxxo.svg" alt="oxo icon" />
+                            <img className="card__seven" src="https://cms.volaris.com/globalassets/1nextgen/payments/seven.svg" alt="seven icon" />
+                            <img className="card_walmark" src="https://cms.volaris.com/globalassets/1nextgen/payments/walmart.svg" alt="walmart icon" />
+                            <img className="card_famahorro" src=" https://cms.volaris.com/globalassets/1nextgen/payments/famahorro.svg" alt="famahorro card" />
+                        </Stack>
+                    </div>
+                </Stack>
+            </section >
+            <Stack className="mainHome__containerCards" direction='column'>
+                <h2>Servicios disponibles</h2>
+                <SimpleGrid spacing={2} templateColumns='repeat(auto-fill, minmax(200px, 1fr))'>
+                    <Card className= "card" w="75%">
                         <CardHeader>
-                            <Heading size='md'> Customer dashboard</Heading>
+                            <Heading size='md'> 
+                            <img src="https://cms.volaris.com/globalassets/1nextgen/externalproducts/hovertransportation.svg" alt="cart icon"/></Heading>
                         </CardHeader>
                         <CardBody>
-                            <Text>View a summary of all your customers over the last month.</Text>
+                            <Text className="cardTittle">Transporte</Text>
                         </CardBody>
                         <CardFooter>
-                            <Button>View here</Button>
+                        <Text className="cardP"> Renta un auto o reserva un shuttle. </Text>
                         </CardFooter>
                     </Card>
-                    <Card>
+                    <Card className= "card" w="75%">
                         <CardHeader>
-                            <Heading size='md'> Customer dashboard</Heading>
+                            <Heading size='md'> 
+                            <img src="https://cms.volaris.com/globalassets/1nextgen/externalproducts/hoveryavas.svg" alt="yavas icon"/></Heading>
                         </CardHeader>
                         <CardBody>
-                            <Text>View a summary of all your customers over the last month.</Text>
+                            <Text className="cardTittle">Vuelo + Hotel</Text>
                         </CardBody>
                         <CardFooter>
-                            <Button>View here</Button>
+                        <Text  className="cardP"> Encuentra las mejores ofertas para tu viaje. </Text>
                         </CardFooter>
                     </Card>
-                    <Card>
+                    <Card className= "card" w="75%">
                         <CardHeader>
-                            <Heading size='md'> Customer dashboard</Heading>
+                            <Heading size='md'> 
+                            <img src="https://cms.volaris.com/globalassets/1nextgen/externalproducts/hovergroups.svg" alt="group icon"/></Heading>
                         </CardHeader>
                         <CardBody>
-                            <Text>View a summary of all your customers over the last month.</Text>
+                            <Text className="cardTittle">Grupos</Text>
+                            
                         </CardBody>
                         <CardFooter>
-                            <Button>View here</Button>
+                        <Text  className="cardP">Obtén una cotización para grupos de más de 9 personas.</Text>
+                        </CardFooter>
+                    </Card>
+                    <Card className= "card" w="75%">
+                        <CardHeader>
+                            <Heading size='md'> 
+                            <img src="https://cms.volaris.com/globalassets/1nextgen/externalproducts/hoverhotels.svg" alt="bed icon"/></Heading>
+                        </CardHeader>
+                        <CardBody>
+                            <Text className="cardTittle">Hoteles</Text>
+                            
+                        </CardBody>
+                        <CardFooter>
+                        <Text  className="cardP"> Reserva una habitación en cualquier parte del mundo. </Text>
+                        </CardFooter>
+                    </Card>
+                    <Card className= "card" w="75%">
+                        <CardHeader>
+                            <Heading size='md'> 
+                            <img src="https://cms.volaris.com/globalassets/1nextgen/externalproducts/hovercargo.svg" alt="box icon"/></Heading>
+                        </CardHeader>
+                        <CardBody>
+                            <Text className="cardTittle">Carga</Text>
+                            
+                        </CardBody>
+                        <CardFooter>
+                        <Text  className="cardP">Contamos con servicio de carga y mensajería.</Text>
                         </CardFooter>
                     </Card>
                 </SimpleGrid>
-            </section>
-        </div>)
+            </Stack>
+
+        </div >)
 
 }
 
